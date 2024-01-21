6804a6b442b4afa7c11046012f685784432123c9
Harry2166 <harry2166.hq@gmail.com>
2024-01-21 22:39:19 +0800

    feat: TRY to make a 3d camera controller

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,8 +7,20 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+#@onready var pivot = $CameraMount/Camera3D
+#@export var sens = 0.15
+
 func get_coin():
 	print("got coin")
+	
+#func _ready():
+	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+#func _input(event):
+	#if event is InputEventMouseMotion:
+		#pivot.rotate_y(deg_to_rad(-event.relative.x * sens))
+		#pivot.rotation.x -= deg_to_rad(event.relative.y * sens)
+		#pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -29,5 +41,8 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	
 	move_and_slide()
