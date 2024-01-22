e87070a65efba8c3821a5831019240c126db892a
Harry2166 <harry2166.hq@gmail.com>
2024-01-22 21:16:16 +0800

    feat: Some small edits

--- Scripts/metallic_block_negative.gd
+++ Scripts/metallic_block_negative.gd
@@ -2,3 +2,6 @@ extends Metal
 
 func _ready():
 	polarity = true
+	magnetizedMaterial.albedo_color = Color(0.92,0.69,0.13,1.0)
+	normalMaterial.albedo_color = Color(1, 0.173, 0.125,1)
+	potentialMaterial.albedo_color = Color(1, 0.0784314, 0.576471, 1)
