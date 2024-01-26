257c659d0b645aac58904e717cfeb0ec79c71c9e
Harry2166 <harry2166.hq@gmail.com>
2024-01-26 16:42:41 +0800

    feat: ignore the last idea

--- Scripts/ice.gd
+++ Scripts/ice.gd
@@ -6,6 +6,6 @@ func _ready():
 	freeze = true
 
 func _process(delta):
-	if player.ice_ice_tubig and Input.is_action_just_pressed("cancel") and player.cryonis_aim_ray.get_collider() == self:
+	if player.ice_ice_tubig and Input.is_action_just_pressed("cancel") and player.aim_ray.get_collider() == self:
 		player.cryo_blocks -= 1
 		queue_free()
